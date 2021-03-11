1416cf04bbdeb43332e5900e3f7a061230610471
Kaede-Hirota <hirota.timecapsule@gmail.com>
2021-03-11 09:38:00 +0900

    コミット２

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+
+import MyPortfolio from '@/components/MyPortfolio'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MyPortfolio
+  }
 }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+    <ol>
+      <li v-for="(item, index) in items" :key=index>
+         <router-link v-bind:to=item.path>
+            {{ item.title }}
+         </router-link>
+      </li>
+    </ol>  
+    </header>
+</template>
