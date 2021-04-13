364243ece2e64a8e4d29425ce737102b10f27a6a
Kaede-Hirota <hirota.timecapsule@gmail.com>
2021-04-14 04:35:25 +0900

    作品の追加

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -42,7 +42,8 @@ export default {
       message: 'これまで作成した作品を載せていきます。更新予定です',
       works: [
         {name: 'Kaedesportfolio', content: 'このポートフォリオサイトです。就活用に作成しましたがせっかくなのでモダンjsの学習も兼ねてVueを用いて作成しました。', language:'使用言語　 Vue.js HTML/CSS Bootstrap', icon: require('@/assets/samune1.png'), url:''},
-        {name: '神経衰弱ゲーム', content: '一年ほど前にモバイルアプリで簡単なゲームを作ってみようと思いアルゴリズムの勉強も兼ねて作成しました。',language:'使用言語　 Kotlin', icon: require('@/assets/samune2.png'),url:'https://github.com/b1017222/ShinkeiSuijaku', value:'作品はこちらからどうぞ'}
+        {name: '神経衰弱ゲーム', content: '一年ほど前にモバイルアプリで簡単なゲームを作ってみようと思いアルゴリズムの勉強も兼ねて作成しました。',language:'使用言語　 Kotlin', icon: require('@/assets/samune2.png'),url:'https://github.com/b1017222/ShinkeiSuijaku', value:'作品はこちらからどうぞ'},
+        {name: 'Todo-List', content: 'サーバーサイドの仕組みやDockerについてもう一度しっかりと勉強をしたいと思い作成しました。',language:'使用言語　 PHP(Laravel) HTML Docker MySQL', icon: require('@/assets/todolist.png'),url:'https://github.com/b1017222/ShinkeiSuijaku', value:'作品はこちらからどうぞ'}
       ]
     }
   },
@@ -67,6 +68,7 @@ justify-content: center;
 }
 .work-box{
   width:400px;
+  padding:20px;
 }
 .work-box img{
   box-shadow: 0 0 10px 0 rgb(0 0 0 / 30%);
@@ -77,5 +79,6 @@ justify-content: center;
   width:700px;
   height:500px;
   background-color:#FFF;
+
 }
 </style>
